d0ad94983dd1ba042cafd8ed6f4011e8d9ae9f41
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2023-08-21 00:38:13 +0530

    Initial commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+    public static void main(String[] args) {
+        /* Student student=new Student();
+        MathCheat cheat=new MathCheat();
+        student.setMathCheat(cheat);
+        student.cheat();
+        */
+
+        ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
+        Student student=context.getBean("stu",Student.class);
+        student.cheat();
+
+        AnotherStudent anotherStudent=context.getBean("anotherStudent",AnotherStudent.class);
+        anotherStudent.startCheating();
+    }
+}
---
+++ src/main/java/org/example/MathCheat.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class MathCheat {
+
+    public MathCheat() {
+        System.out.println("Constructor is calling");
+    }
+
+    public void cheatOfMath() {
+        System.out.println("Math Cheating is Started....");
+    }
+}
